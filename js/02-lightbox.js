9f172efe8573a142bc51e7464b2d770c131b08cc
Filip Bączkowski <filo.b@wp.pl>
2023-04-09 00:17:43 +0200

    feat: second task completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+function galleryRender() {
+  // Add by Array
+  let galleryArrayItems = [];
+  // Add by HTML
+  // let galleryHtmlItems = "";
+
+  galleryItems.forEach((e, idx) => {
+    const { description, original, preview } = galleryItems[idx];
+
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.classList.add("gallery__item");
+    img.classList.add("gallery__image");
+
+    a.setAttribute("href", original);
+    img.setAttribute("src", preview);
+    img.setAttribute("alt", description);
+
+    li.append(a);
+    a.append(img);
+
+    // Add by Array
+    galleryArrayItems.push(li);
+    // Add by HTML
+    // galleryHtmlItems += li.outerHTML;
+  });
+
+  // Add by Array
+  galleryList.append(...galleryArrayItems);
+  // Add by HTML
+  // galleryList.insertAdjacentHTML("beforeend", galleryHtmlItems);
+}
+
+galleryRender();
+
+var lightbox = new SimpleLightbox(".gallery li a", {
+  captionsData: "alt",
+  captionPosition: "outside",
+  captionDelay: 250,
+});
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.open(event.target);
+});
 
 console.log(galleryItems);
+console.log("lightbox object: ", lightbox);
+console.log(galleryList);
